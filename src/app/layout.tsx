96ce57ad3fae2617aea5222a6f67e86480a20ede
Stopher Malik <malikstopher@gmail.com>
2026-07-15 23:22:56 +0200

    Update favicon to use logo.jpg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,8 +64,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/logo.jpg" sizes="any" />
+        <link rel="apple-touch-icon" href="/logo.jpg" />
       </head>
       <body suppressHydrationWarning>
         <Providers>{children}</Providers>
